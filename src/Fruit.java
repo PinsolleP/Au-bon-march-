2e55b78a88450eb51cafa26ad11517cd403c2696
Paul Pinsolle <paul.pinsolle@fms-ea.com>
2026-08-19 11:24:11 +0200

    Création classe Fruit constructeur et méthodes

---
+++ src/Fruit.java
@@ -0,0 +1,34 @@
+import java.time.LocalDate;
+import java.time.chrono.ChronoLocalDate;
+import java.time.temporal.ChronoUnit;
+
+public class Fruit extends Product implements Consumable{
+
+    //attributs
+    private int daysToRipen = 3 ;
+
+    public Fruit(String name, double unitPrice, double stockQuantity, String unite, LocalDate pickingDate, int shelfLifeDays){
+        super(name, unitPrice, stockQuantity, unite, pickingDate, shelfLifeDays);
+    }
+
+    @Override
+    public LocalDate calculateExpirationDate() {
+        return pickingDate.plusDays(shelfLifeDays);
+    }
+
+
+    @Override
+    public boolean isRipe() {
+        return LocalDate.now().isAfter(pickingDate.plusDays(daysToRipen));
+    }
+
+    @Override
+    public boolean isExpired(LocalDate dateVerification) {
+        return dateVerification.isAfter(calculateExpirationDate());
+    }
+
+    @Override
+    public long daysRemainingBeforeExpiration(LocalDate dateVerification) {
+        return ChronoUnit.DAYS.between(dateVerification, calculateExpirationDate());
+    }
+}
