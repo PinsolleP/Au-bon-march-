1e7e9cd46757a229bcf1d59a70e283eaf5be7581
Paul Pinsolle <paul.pinsolle@fms-ea.com>
2026-08-19 09:20:49 +0200

    Création class Product, Attributs, constructeur et accesseurs

--- src/Product.java
+++ src/Product.java
@@ -1,3 +1,74 @@
+import java.time.LocalDate;
+
 public class Product {
-    
+
+    public abstract class Product {
+
+        //attributs
+        public String name;
+        public double unitPrice;
+        public double stockQuantity;
+        public String unite;
+        public LocalDate pickingDate;
+        public int shelfLifeDays;
+
+        public Product(String name, double unitPrice, double stockQuantity, String unite, LocalDate pickingDate, int shelfLifeDays){
+            this.name = name;
+            this.unitPrice = unitPrice;
+            this.unite = unite;
+            this.stockQuantity = stockQuantity;
+            this.pickingDate = pickingDate;
+            this.shelfLifeDays = shelfLifeDays;
+        }
+
+        //Accesseurs
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public double getUnitPrice() {
+            return unitPrice;
+        }
+
+        public void setUnitPrice(double unitPrice) {
+            this.unitPrice = unitPrice;
+        }
+
+        public double getStockQuantity() {
+            return stockQuantity;
+        }
+
+        public void setStockQuantity(double stockQuantity) {
+            this.stockQuantity = stockQuantity;
+        }
+
+        public String getUnite() {
+            return unite;
+        }
+
+        public void setUnite(String unite) {
+            this.unite = unite;
+        }
+
+        public LocalDate getPickingDate() {
+            return pickingDate;
+        }
+
+        public void setPickingDate(LocalDate pickingDate) {
+            this.pickingDate = pickingDate;
+        }
+
+        public int getShelfLifeDays() {
+            return shelfLifeDays;
+        }
+
+        public void setShelfLifeDays(int shelfLifeDays) {
+            this.shelfLifeDays = shelfLifeDays;
+        }
+    }
 }
